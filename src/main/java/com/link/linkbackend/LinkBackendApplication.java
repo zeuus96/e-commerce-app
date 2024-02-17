9fe8e6b4d95ab617dfd55fc106838641cf6af257
hamza <hamzabr01@outlook.fr>
2024-02-17 23:31:18 +0100

    generate project

---
+++ src/main/java/com/link/linkbackend/LinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.link.linkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkBackendApplication.class, args);
+    }
+
+}
